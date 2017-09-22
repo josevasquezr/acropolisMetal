32e7109081cbaa1b87196f8c3f1e1c2e9ea15557
josevasquezr <javierv.ramos@gmail.com>
2017-09-21 23:15:56 -0600

    Finalizado nav, article, footer de landing page

--- js/formRegistro.js
+++ js/formRegistro.js
@@ -1,40 +1,40 @@
 $(function(){
-	$(".textbox").focus(function(){
-		var label = $(this).siblings().eq(0);
-		if ($(this).val() == "") {
-			label.toggleClass("labelActive");
-		}
-	});
+	// $(".textbox").focus(function(){
+	// 	var label = $(this).siblings().eq(0);
+	// 	if ($(this).val() == "") {
+	// 		label.toggleClass("labelActive");
+	// 	}
+	// });
 
-	$(".textbox").blur(function(){
-		var label = $(this).siblings().eq(0);
-		if ($(this).val() == "") {
-			label.toggleClass("labelActive");
-		}
-	});
+	// $(".textbox").blur(function(){
+	// 	var label = $(this).siblings().eq(0);
+	// 	if ($(this).val() == "") {
+	// 		label.toggleClass("labelActive");
+	// 	}
+	// });
 
-	var abierto = 0;
-	$(".menu").click(function(){
-		if (abierto == 0) {
-			$("nav").addClass("abrir");
-			$("nav").removeClass("cerrar");
-			abierto = 1;
-		}else{
-			$("nav").removeClass("abrir");
-			$("nav").addClass("cerrar");
-			abierto = 0;
-		}
-	});
+	// var abierto = 0;
+	// $(".menu").click(function(){
+	// 	if (abierto == 0) {
+	// 		$("nav").addClass("abrir");
+	// 		$("nav").removeClass("cerrar");
+	// 		abierto = 1;
+	// 	}else{
+	// 		$("nav").removeClass("abrir");
+	// 		$("nav").addClass("cerrar");
+	// 		abierto = 0;
+	// 	}
+	// });
 
-	$("nav").click(function(){
-		if (abierto == 0) {
-			$("nav").addClass("abrir");
-			$("nav").removeClass("cerrar");
-			abierto = 1;
-		}else{
-			$("nav").removeClass("abrir");
-			$("nav").addClass("cerrar");
-			abierto = 0;
-		}
-	});
+	// $("nav").click(function(){
+	// 	if (abierto == 0) {
+	// 		$("nav").addClass("abrir");
+	// 		$("nav").removeClass("cerrar");
+	// 		abierto = 1;
+	// 	}else{
+	// 		$("nav").removeClass("abrir");
+	// 		$("nav").addClass("cerrar");
+	// 		abierto = 0;
+	// 	}
+	// });
 });
